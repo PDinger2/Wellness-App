e12a675d9dc52ca6c0c38a7fa109dd73be832807
tbraud03 <trb039@email.latech.edu>
2026-07-06 22:26:41 -0500

    begin component development

--- src/app/habits.tsx
+++ src/app/habits.tsx
@@ -1,23 +1,7 @@
-import { HStack } from "@/components/ui/hstack";
-import { styles } from "@/constants/styles";
-import { useEffect, useState } from "react";
-import { Text } from "react-native-paper";
-import { SafeAreaView } from "react-native-safe-area-context";
+import HabitsScreen from "@/components/habits/HabitsScreen";
 
-export default function HabitsScreen() {
-  const [ habitsComplete, setHabitsComplete ] = useState(0)
-  const [ habitNumber, setHabitNumber ] = useState(0)
-  useEffect(() => {
-    setHabitNumber(3)
-  }, [])
-    return (
-        <>
-          <SafeAreaView style={styles.safeArea}>
-                <HStack style={styles.headerRow}>
-                    <Text variant="headlineLarge">Habits</Text>
-                    <Text variant="titleMedium">{habitsComplete}/{habitNumber} complete</Text>
-                </HStack>
-            </SafeAreaView>
-        </>
-    )
+export default function habits() {
+  return (
+    <HabitsScreen />
+  )
 }
---
+++ src/components/habits/WeeklyView.tsx
@@ -0,0 +1,10 @@
+import { styles } from "@/constants/styles";
+import { HStack } from "../ui/hstack";
+import { Text, Button, Avatar, TouchableRipple, useTheme } from "react-native-paper"
+import { useState, useEffect } from "react";
+
+export function WeekStrip() {
+    const days = [1, 2, 3, 4, 5, 6, 7] // hardcoded for now, will grab date automatically later
+    const [ dailyData, setDailyData ] = useState 
+    )
+}
